06365f4a9d61d469259dafcaabc752f05780569f
unknown <lotfe006@gmail.com>
2021-09-08 14:06:02 +0300

    first

---
+++ المشروع/Form1.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace المشروع
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbt = new System.Windows.Forms.Label();
+            this.Display = new System.Windows.Forms.Button();
+            this.Hide = new System.Windows.Forms.Button();
+            this.Check = new System.Windows.Forms.Button();
+            this.NewGame = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tx4 = new System.Windows.Forms.TextBox();
+            this.tx3 = new System.Windows.Forms.TextBox();
+            this.tx2 = new System.Windows.Forms.TextBox();
+            this.tx1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lb4 = new System.Windows.Forms.Label();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.lb2 = new System.Windows.Forms.Label();
+            this.lb3 = new System.Windows.Forms.Label();
+            this.tm = new System.Windows.Forms.Timer(this.components);
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbt
+            // 
+            this.lbt.AutoSize = true;
+            this.lbt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbt.Location = new System.Drawing.Point(217, 30);
+            this.lbt.Name = "lbt";
+            this.lbt.Size = new System.Drawing.Size(63, 25);
+            this.lbt.TabIndex = 0;
+            this.lbt.Text = "Time";
+            this.lbt.Click += new System.EventHandler(this.lbt_Click);
+            // 
+            // Display
+            // 
+            this.Display.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Display.Location = new System.Drawing.Point(382, 107);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(75, 34);
+            this.Display.TabIndex = 1;
+            this.Display.Text = "LooK";
+            this.Display.UseVisualStyleBackColor = true;
+            this.Display.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Hide
+            // 
+            this.Hide.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Hide.Location = new System.Drawing.Point(382, 163);
+            this.Hide.Name = "Hide";
+            this.Hide.Size = new System.Drawing.Size(75, 34);
+            this.Hide.TabIndex = 1;
+            this.Hide.Text = "Hide";
+            this.Hide.UseVisualStyleBackColor = true;
+            this.Hide.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Check
+            // 
+            this.Check.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Check.Location = new System.Drawing.Point(382, 262);
+            this.Check.Name = "Check";
+            this.Check.Size = new System.Drawing.Size(75, 34);
+            this.Check.TabIndex = 1;
+            this.Check.Text = "Check";
+            this.Check.UseVisualStyleBackColor = true;
+            this.Check.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // NewGame
+            // 
+            this.NewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewGame.Location = new System.Drawing.Point(9, 84);
+            this.NewGame.Name = "NewGame";
+            this.NewGame.Size = new System.Drawing.Size(126, 42);
+            this.NewGame.TabIndex = 1;
+            this.NewGame.Text = "New Game";
+            this.NewGame.UseVisualStyleBackColor = true;
+            this.NewGame.Click += new System.EventHandler(this.NewGame_click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(8, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 30);
+            this.label2.TabIndex = 0;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(7, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 30);
+            this.label3.TabIndex = 0;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tx4);
+            this.groupBox2.Controls.Add(this.tx3);
+            this.groupBox2.Controls.Add(this.tx2);
+            this.groupBox2.Controls.Add(this.tx1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(153, 236);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox2.Size = new System.Drawing.Size(200, 77);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Suggestions";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // tx4
+            // 
+            this.tx4.Location = new System.Drawing.Point(159, 36);
+            this.tx4.MaxLength = 1;
+            this.tx4.Name = "tx4";
+            this.tx4.Size = new System.Drawing.Size(35, 26);
+            this.tx4.TabIndex = 0;
+            this.tx4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tx4.TextChanged += new System.EventHandler(this.tx4_TextChanged);
+            this.tx4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tx4_KeyPress);
+            // 
+            // tx3
+            // 
+            this.tx3.Location = new System.Drawing.Point(110, 36);
+            this.tx3.MaxLength = 1;
+            this.tx3.Name = "tx3";
+            this.tx3.Size = new System.Drawing.Size(35, 26);
+            this.tx3.TabIndex = 0;
+            this.tx3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tx3.TextChanged += new System.EventHandler(this.tx3_TextChanged);
+            this.tx3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tx3_KeyPress);
+            // 
+            // tx2
+            // 
+            this.tx2.Location = new System.Drawing.Point(60, 36);
+            this.tx2.MaxLength = 1;
+            this.tx2.Name = "tx2";
+            this.tx2.Size = new System.Drawing.Size(35, 26);
+            this.tx2.TabIndex = 0;
+            this.tx2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tx2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.tx2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tx2_KeyPress);
+            // 
+            // tx1
+            // 
+            this.tx1.Location = new System.Drawing.Point(9, 36);
+            this.tx1.MaxLength = 1;
+            this.tx1.Name = "tx1";
+            this.tx1.Size = new System.Drawing.Size(35, 26);
+            this.tx1.TabIndex = 0;
+            this.tx1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tx1.TextChanged += new System.EventHandler(this.tx1_TextChanged);
+            this.tx1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tx1_KeyPress);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(4, 229);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(143, 135);
+            this.panel1.TabIndex = 4;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(51, 94);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(88, 15);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Wrong Place";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(54, 54);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 15);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "True";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(55, 17);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 15);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "False";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.LimeGreen;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(7, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 30);
+            this.label6.TabIndex = 0;
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lb4);
+            this.groupBox1.Controls.Add(this.lb1);
+            this.groupBox1.Controls.Add(this.lb2);
+            this.groupBox1.Controls.Add(this.lb3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(153, 99);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox1.Size = new System.Drawing.Size(200, 79);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hidden Numbers";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lb4
+            // 
+            this.lb4.BackColor = System.Drawing.SystemColors.Control;
+            this.lb4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lb4.Location = new System.Drawing.Point(159, 34);
+            this.lb4.Name = "lb4";
+            this.lb4.Size = new System.Drawing.Size(35, 25);
+            this.lb4.TabIndex = 0;
+            this.lb4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lb1
+            // 
+            this.lb1.BackColor = System.Drawing.SystemColors.Control;
+            this.lb1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lb1.Location = new System.Drawing.Point(9, 34);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(35, 25);
+            this.lb1.TabIndex = 0;
+            this.lb1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lb1.Click += new System.EventHandler(this.lb1_Click);
+            // 
+            // lb2
+            // 
+            this.lb2.BackColor = System.Drawing.SystemColors.Control;
+            this.lb2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lb2.Location = new System.Drawing.Point(60, 34);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(35, 25);
+            this.lb2.TabIndex = 0;
+            this.lb2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lb3
+            // 
+            this.lb3.BackColor = System.Drawing.SystemColors.Control;
+            this.lb3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lb3.Location = new System.Drawing.Point(110, 34);
+            this.lb3.Name = "lb3";
+            this.lb3.Size = new System.Drawing.Size(35, 25);
+            this.lb3.TabIndex = 0;
+            this.lb3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tm
+            // 
+            this.tm.Enabled = true;
+            this.tm.Interval = 1150;
+            this.tm.Tick += new System.EventHandler(this.tm_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 376);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.NewGame);
+            this.Controls.Add(this.Check);
+            this.Controls.Add(this.Hide);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.lbt);
+            this.Name = "Form1";
+            this.Text = " 5";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbt;
+        private System.Windows.Forms.Button Display;
+        private System.Windows.Forms.Button Hide;
+        private System.Windows.Forms.Button Check;
+        private System.Windows.Forms.Button NewGame;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tx4;
+        private System.Windows.Forms.TextBox tx3;
+        private System.Windows.Forms.TextBox tx2;
+        private System.Windows.Forms.TextBox tx1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lb4;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Label lb2;
+        private System.Windows.Forms.Label lb3;
+        private System.Windows.Forms.Timer tm;
+    }
+}
+
---
+++ المشروع/Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace المشروع
+{
+    public partial class Form1 : Form
+
+         
+
+
+    {
+        public SoundPlayer tmsound;
+       
+        public SoundPlayer game;
+
+        public SoundPlayer SWINN;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            SWINN = new SoundPlayer("SWIN.WAV");
+            tmsound = new SoundPlayer("countdown.WAV");
+            game = new SoundPlayer ("Game1.WAV");
+           
+           
+        }
+       
+
+         
+        public void hiddingnumbers()
+        {
+            foreach (Label item in this.groupBox1.Controls)
+            {
+                item.Text = "";
+            }
+        }
+
+        
+
+     
+
+       public void show()
+        {
+            lb1.Text = num1.ToString();
+            lb2.Text = num2.ToString();
+            lb3.Text = num3.ToString();
+            lb4.Text = num4.ToString();
+        }
+       
+        public void GetNumbers()
+        {
+             Random r = new Random();
+            while (true)
+            {
+                num1 = r.Next(0, 9);
+                num2 = r.Next(0, 9);
+                num3 = r.Next(0, 9);
+                num4 = r.Next(0, 9);
+
+                if( num1 !=num2 && num1!=num3 && num1!=num4 && num2!=num3 && num2!=num4 && num3!=num4 )
+                {
+                    break;
+                }
+            }
+        }
+       
+        public void DeleteNum()
+        {
+            foreach (TextBox item in this.groupBox2.Controls)
+            {
+                item.Text = " ";
+                item.BackColor = Color.White;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        int num1, num2, num3, num4;
+        private void NewGame_click(object sender, EventArgs e)
+        {
+            
+            GetNumbers();
+            hiddingnumbers();
+            DeleteNum();
+            Display.Enabled = true;
+            Hide.Enabled = true;
+            Check.Enabled = true;
+            tx1.Enabled = true;
+            tx2.Enabled = true;
+            tx3.Enabled = true;
+            tx4.Enabled = true;
+            this.BackColor =Color.FromName("Control");
+            
+            i = 30;
+            tm.Start();
+           
+                tmsound.Stop();
+             
+           
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            hiddingnumbers();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //game.Play();
+            GetNumbers();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           //----------------tx1
+            if (tx1.Text == num1.ToString())
+            {
+                tx1.BackColor = Color.Green;
+            }
+            else if (tx1.Text == num2.ToString() || tx1.Text == num3.ToString() || tx1.Text == num4.ToString())
+            {
+                tx1.BackColor = Color.Yellow;
+            }
+            else
+            {
+                tx1.BackColor = Color.Red;
+            }
+
+            //----------------tx2
+            if (tx2.Text == num2.ToString())
+            {
+                tx2.BackColor = Color.Green;
+            }
+            else if (tx2.Text == num1.ToString() || tx2.Text == num3.ToString() || tx2.Text == num4.ToString())
+            {
+                tx2.BackColor = Color.Yellow;
+            }
+            else
+            {
+                tx2.BackColor = Color.Red;
+            }
+
+            //----------------tx3
+            if (tx3.Text == num3.ToString())
+            {
+                tx3.BackColor = Color.Green;
+            }
+            else if (tx3.Text == num1.ToString() || tx3.Text == num2.ToString() || tx3.Text == num4.ToString())
+            {
+                tx3.BackColor = Color.Yellow;
+            }
+            else
+            {
+                tx3.BackColor = Color.Red;
+            }
+            
+                  //----------------tx4
+            if (tx4.Text == num4.ToString())
+            {
+                tx4.BackColor = Color.Green;
+            }
+            else if (tx4.Text == num1.ToString() || tx4.Text == num3.ToString() || tx4.Text == num2.ToString())
+            {
+                tx4.BackColor = Color.Yellow;
+            }
+            else
+            {
+                tx4.BackColor = Color.Red;
+            }
+
+            if (tx1.Text==num1.ToString() && tx2.Text==num2.ToString() && tx3.Text==num3.ToString () && tx4.Text==num4.ToString() )
+            {
+                game.Stop();
+               
+                
+                tmsound.Stop();
+                SWINN.Play();
+                show();
+                tm.Stop();
+                MessageBox.Show(" GREAT ");
+                Display.Enabled = false;
+                Hide.Enabled = false;
+                Check.Enabled = false;
+                tx1.Enabled = false;
+                tx2.Enabled = false;
+                tx3.Enabled = false;
+                tx4.Enabled = false;
+
+            }
+            }
+
+
+        private void lb1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+        int i = 30;
+       
+            
+    
+        private void tm_Tick(object sender, EventArgs e)
+        {
+            i--;
+            lbt.Text = i.ToString();
+
+            while(i==29)
+            {
+                game.Play();
+                break;
+            }
+           
+
+            if (i == 6)
+
+            {
+                game.Stop();
+                tmsound.Play();
+             
+               
+            }
+           
+           
+            
+            if (i == 0)
+            {
+                tm.Stop();
+                this.BackColor = Color.Red;
+                MessageBox.Show(" Time is Up");
+                Display.Enabled = false;
+                Hide.Enabled = false;
+                Check.Enabled = false;
+                tx1.Enabled = false;
+                tx2.Enabled = false;
+                tx3.Enabled = false;
+                tx4.Enabled = false;
+                show();
+
+            }
+        }
+
+        private void lbt_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void tx1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char chr = e.KeyChar;
+            if (!char.IsDigit(chr) && chr != 8)
+            {
+                e.Handled = true;
+                MessageBox.Show(" Please Enter Valid Value ");
+            }
+        }
+
+        private void tx2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char chr = e.KeyChar;
+            if (!char.IsDigit(chr) && chr != 8)
+            {
+                e.Handled = true;
+                MessageBox.Show(" Please Enter Valid Value ");
+            }
+        }
+
+        private void tx3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char chr = e.KeyChar;
+            if (!char.IsDigit(chr) && chr != 8)
+            {
+                e.Handled = true;
+                MessageBox.Show(" Please Enter Valid Value ");
+            }
+        }
+
+        private void tx4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char chr = e.KeyChar;
+            if (!char.IsDigit(chr) && chr != 8)
+            {
+                e.Handled = true;
+                MessageBox.Show(" Please Enter Valid Value ");
+            }
+        }
+
+        private void tx3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tx1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tx4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public SoundPlayer win_ { get; set; }
+    }
+}
